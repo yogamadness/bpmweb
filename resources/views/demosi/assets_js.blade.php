69d9e6b824820322372c01c5aa6998d5ed55189d
Hendro Utomo <hendroutomo@gmail.com>
2016-11-12 05:01:16 +0700

    no message

--- resources/views/demosi/assets_js.blade.php
+++ resources/views/demosi/assets_js.blade.php
@@ -246,7 +246,7 @@ $(document).ready(function(){
         limit: 10,
         source: nik
     })
-    .on('typeahead:selected', getDataEmpProd);
+    .on('typeahead:selected', getDataEmp);
 });
 
 </script>
