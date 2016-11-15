dd66321309dd87840bbf2faff0c9b9a8b032098a
Yoga Pratama <yogamadness@gmail.com>
2016-11-15 20:32:16 +0700

    form ui edit
    form ui edit

--- resources/views/recruitment/edit.blade.php
+++ resources/views/recruitment/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts-master.master')
 
-@section('tittle', 'Input Karyawan Baru')
+@section('title', 'Perubahan Data Karyawan ')
 
 @section('cascanding')
 <link rel="stylesheet" href="dist/plugins/datetimepicker/css/bootstrap-datetimepicker.min.css">
@@ -16,31 +16,452 @@
   </div>
 </div>
 <div class="row">
-  <div class="col-md-12">
-    <!-- general form elements -->
-    <div class="box box-success"  id="formtambah">
-      <!-- Permintaan Tenaga Kerja -->
-      <div class="box-header with-border">
-        <h3 class="box-title"><b>Permintaan Tenaga Kerja</b></h3>
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+              <label for="nik_nasional">
+                Nik Nasional
+              </label>
+              <input type="input" class="form-control" id="nik_nasional" />
+            </div>
+            <div class="form-group"> 
+              <label for="Nik Sap">
+                Nik Sap
+              </label>
+              <input type="input" class="form-control" id="nik_sap" />
+            </div>
+            <div class="form-group"> 
+              <label for="Nama Karyawan">
+                Nama Karyawan
+              </label>
+              <input type="input" class="form-control" id="nama_karyawan" />
+            </div>
+            <div class="form-group"> 
+              <label for="Jabatan">
+                Jabatan
+              </label>
+              <input type="input" class="form-control" id="jabatan" />
+            </div>
+            <div class="form-group"> 
+              <label for="tanggal">
+                 Tanggal Aktif Kerja
+              </label>
+              <input type="date" class="form-control" id="tanggal_aktif" />
+            </div>
+        </div>
+        <div class="col-md-6">
+          <img alt="Foto" src="http://lorempixel.com/140/140/" class="img-thumbnail" /> 
+          <span class="label label-default">Ganti Foto</span>
+        </div>
       </div>
-      <!-- /.box-header -->
-      <div class="box-body">
-        <form enctype="multipart/form-data" id="formFptk" class="form-horizontal" name="formFptk">
-          <input type="hidden" name="tipePtk" id="tipePtk">
-         
-          <div class="fptk">
-
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-3">
+               <span class="label label-default">Ganti Ktp</span>
+            </div>
+            <div class="col-md-3">
+               <span class="label label-default">Ganti Kk</span>
+            </div>
+            <div class="col-md-3">
+               <span class="label label-default">Ganti Npwp</span>
+            </div>
+            <div class="col-md-3">
+               <span class="label label-default">Ganti Sim</span>
+            </div>
           </div>
-         
-        </form>
+        </div>
       </div>
-      <!-- /.box-body -->
-      <div class="box-footer">
-      <button class="btn btn-default" type="submit">Batal</button>
-      <button class="btn btn-success pull-right" type="button" id="ajukan">Ajukan</button>
-      </div><!-- /.box-footer -->
+
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-6">
+              <form role="form">
+                <div class="form-group">
+                   
+                  <label for="no_ktp">
+                    No KTP
+                  </label>
+                  <input type="input" class="form-control" id="no_ktp" />
+                </div>
+                <div class="form-group">
+                  <label for="no_npwp">
+                     No Npwp
+                  </label>
+                  <input type="input" class="form-control" id="no_npwp" />
+                </div>
+                <div class="form-group">
+                  <label for="mama">
+                     Nama Gadis Ibu Kandung
+                  </label>
+                  <input type="input" class="form-control" id="mama" />
+                </div>
+                <div class="form-group">
+                  <label for="nama_lengkap">
+                     Nama Lengkap
+                  </label>
+                  <input type="input" class="form-control" id="nama_lengkap" />
+                </div>
+                <div class="form-group">
+                  <label for="agama">
+                     Agama
+                  </label>
+                  <input type="input" class="form-control" id="agama" />
+                </div>
+
+                <div class="form-group">
+                  <label for="tempat_lahir">
+                    Tempat Lahir
+                  </label>
+                  <input type="input" class="form-control" id="tempat_lahir"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="jenis_kelamin">
+                    Jenis Kelamin
+                  </label>
+                  
+                   <input  id = "sex" type="radio" name ="gender"  value ="L"/> L
+                   <input id = "sex" type="radio" name="gender"    value="P"/>   P  
+                </div>
+
+              </form>
+            </div>
+            <div class="col-md-6">
+              <form role="form">
+                <div class="form-group">
+                   
+                  <label for="exampleInputEmail1">
+                    Email address
+                  </label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" />
+                </div>
+                <div class="form-group">
+                   
+                  <label for="exampleInputPassword1">
+                    Password
+                  </label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <div class="form-group">
+                   
+                  <label for="exampleInputFile">
+                    File input
+                  </label>
+                  <input type="file" id="exampleInputFile" />
+                  <p class="help-block">
+                    Example block-level help text here.
+                  </p>
+                </div>
+                <div class="checkbox">
+                   
+                  <label>
+                    <input type="checkbox" /> Check me out
+                  </label>
+                </div> 
+                <button type="submit" class="btn btn-default">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+               <span class="label label-default">Label</span>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>
+                      #
+                    </th>
+                    <th>
+                      Product
+                    </th>
+                    <th>
+                      Payment Taken
+                    </th>
+                    <th>
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      01/04/2012
+                    </td>
+                    <td>
+                      Default
+                    </td>
+                  </tr>
+                  <tr class="active">
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      01/04/2012
+                    </td>
+                    <td>
+                      Approved
+                    </td>
+                  </tr>
+                  <tr class="success">
+                    <td>
+                      2
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      02/04/2012
+                    </td>
+                    <td>
+                      Declined
+                    </td>
+                  </tr>
+                  <tr class="warning">
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      03/04/2012
+                    </td>
+                    <td>
+                      Pending
+                    </td>
+                  </tr>
+                  <tr class="danger">
+                    <td>
+                      4
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      04/04/2012
+                    </td>
+                    <td>
+                      Call in to confirm
+                    </td>
+                  </tr>
+                </tbody>
+              </table> 
+              <button type="button" class="btn btn-default">
+                Default
+              </button> <span class="label label-default">Label</span>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>
+                      #
+                    </th>
+                    <th>
+                      Product
+                    </th>
+                    <th>
+                      Payment Taken
+                    </th>
+                    <th>
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      01/04/2012
+                    </td>
+                    <td>
+                      Default
+                    </td>
+                  </tr>
+                  <tr class="active">
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      01/04/2012
+                    </td>
+                    <td>
+                      Approved
+                    </td>
+                  </tr>
+                  <tr class="success">
+                    <td>
+                      2
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      02/04/2012
+                    </td>
+                    <td>
+                      Declined
+                    </td>
+                  </tr>
+                  <tr class="warning">
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      03/04/2012
+                    </td>
+                    <td>
+                      Pending
+                    </td>
+                  </tr>
+                  <tr class="danger">
+                    <td>
+                      4
+                    </td>
+                    <td>
+                      TB - Monthly
+                    </td>
+                    <td>
+                      04/04/2012
+                    </td>
+                    <td>
+                      Call in to confirm
+                    </td>
+                  </tr>
+                </tbody>
+              </table> 
+              <button type="button" class="btn btn-default">
+                Default
+              </button> <span class="label label-default">Label</span>
+              <div class="row">
+                <div class="col-md-6">
+                  <form role="form">
+                    <div class="form-group">
+                       
+                      <label for="exampleInputEmail1">
+                        Email address
+                      </label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" />
+                    </div>
+                    <div class="form-group">
+                       
+                      <label for="exampleInputPassword1">
+                        Password
+                      </label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div class="form-group">
+                       
+                      <label for="exampleInputFile">
+                        File input
+                      </label>
+                      <input type="file" id="exampleInputFile" />
+                      <p class="help-block">
+                        Example block-level help text here.
+                      </p>
+                    </div>
+                    <div class="checkbox">
+                       
+                      <label>
+                        <input type="checkbox" /> Check me out
+                      </label>
+                    </div> 
+                    <button type="submit" class="btn btn-default">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+                <div class="col-md-6">
+                  <form role="form">
+                    <div class="form-group">
+                       
+                      <label for="exampleInputEmail1">
+                        Email address
+                      </label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" />
+                    </div>
+                    <div class="form-group">
+                       
+                      <label for="exampleInputPassword1">
+                        Password
+                      </label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div class="form-group">
+                       
+                      <label for="exampleInputFile">
+                        File input
+                      </label>
+                      <input type="file" id="exampleInputFile" />
+                      <p class="help-block">
+                        Example block-level help text here.
+                      </p>
+                    </div>
+                    <div class="checkbox">
+                       
+                      <label>
+                        <input type="checkbox" /> Check me out
+                      </label>
+                    </div> 
+                    <button type="submit" class="btn btn-default">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4">
+                   
+                  <button type="button" class="btn btn-sm">
+                    Default
+                  </button>
+                </div>
+                <div class="col-md-4">
+                   
+                  <button type="button" class="btn btn-sm">
+                    Default
+                  </button>
+                </div>
+                <div class="col-md-4">
+                   
+                  <button type="button" class="btn btn-sm">
+                    Default
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+</div>
 </div>
 @endsection
 
