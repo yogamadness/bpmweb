c78b2b5d6e2a2e62b731cbbb9eb1da0c77092b6e
Yoga Pratama <yogamadness@gmail.com>
2016-11-17 01:27:37 +0700

    peach ui edit
    pecah ui edit

---
+++ resources/views/recruitment/editbaru.blade.php
@@ -0,0 +1,297 @@
+<div class="col-md-6">
+                                <div class="form-group">
+                     
+                    <label for="no_ktp_br" class="col-sm-4 ">
+                      No KTP
+                    </label>
+                    <div class="col-sm-10"> 
+                      <input type="input" class="form-control" id="no_ktp_br" />
+                    </div>
+                    
+                </div>
+                <div class="form-group">
+                    <label for="no_npwp" class="col-sm-4 ">
+                       No NPWP
+                    </label>
+
+                    <div class="col-sm-10"> 
+                     <input type="input" class="form-control" id="no_npwp_br" />   
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="no_npwp" class="col-sm-4 ">
+                       No SIM
+                    </label>
+
+                    <div class="col-sm-10"> 
+                     <input type="input" class="form-control" id="no_npwp_br" />   
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="mama" class="col-sm-6">
+                       Nama Gadis Ibu Kandung
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" class="form-control" id="mama_br" />
+                    </div>
+                    
+                  </div>
+                  <div class="form-group">
+                    <label for="nama_lengkap" class="col-sm-6">
+                       Nama Lengkap
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" class="form-control" id="nama_lengkap_br" />  
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="agama" class="col-sm-6">
+                       Agama
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" class="form-control" id="agama_br" />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="tempat_lahir" class="col-sm-4">
+                      Tempat Lahir
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="input" class="form-control" id="tempat_lahir_br" />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="jenis_kelamin" class="col-sm-4">
+                      Jenis Kelamin
+                    </label>
+
+                    <div class="col-sm-10">
+                         <input  id = "sex" type="radio" name ="gender"  value ="L_br" /> L
+                         <input id = "sex" type="radio" name="gender"    value="P_br" />   P  
+                    </div>
+                  
+                  </div>
+
+                   <div class="form-group">
+                    <label for="gol_dar" class="col-sm-4">
+                      Golongan Darah
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" class="form-control" id="gol_dar_br" />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="tinggi" class="col-sm-4">
+                      Tinggi Badan
+                    </label>
+
+                    <div class="col-sm-10">
+                      <div class="input-group">
+                        <input id ="tinggi_br" type="text" class="form-control" placeholder="" aria-describedby="basic-addon2"  />
+                        <span class="input-group-addon" id="basic-addon2">cm</span>
+                    </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="berat" class="col-sm-4">
+                      Berat Badan
+                    </label>
+                    
+                    <div class="col-sm-10">
+                        <div class="input-group">
+                          <input id ="berat_br" type="text" class="form-control" placeholder="" aria-describedby="basic-addon2"  />
+                          <span class="input-group-addon" id="basic-addon2">kg</span>
+                        </div>
+                    </div>
+                  </div>
+
+
+                  <div class="form-group">
+                    <label for="tgl_lahir" class="col-sm-4">
+                      Tanggal Lahir
+                    </label>
+
+                    <div class="col-sm-10">
+                      <input type="date" class="form-control" id="tgl_lahir_br" />
+                    </div>
+                  </div>
+
+
+                  <div class="form-group">
+                    <label for="alamat_ktp" class="col-sm-6">
+                      Alamat Sesuai Ktp
+                    </label>
+
+                    <div class="col-sm-10">
+                       <textarea id="alamat_ktp_br" class="form-control" rows="3"  /></textarea>
+                    </div>
+
+                  </div>
+
+                  <div class="form-group">
+                    <label for="lurah_ktp" class="col-sm-6">
+                      Kelurahan Sesuai Ktp
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="input" id="lurah_ktp_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="kota_ktp" class="col-sm-6">
+                      Kota Sesuai Ktp
+                    </label>
+
+                    <div class="col-sm-10">
+                        <input type="input" id="kota_ktp_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="telp_ktp" class="col-sm-6">
+                      No Telepon Alamat Sesuai Ktp
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="tel" id="telp_ktp_br" class="form-control"  />
+                    </div>
+                    
+                  </div>
+
+
+                  <div class="form-group">
+                    <label for="kec_ktp" class="col-sm-6"> 
+                      Kecamatan Sesuai Ktp
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" id="kec_ktp_br" class="form-control"  />  
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="kodepos_ktp" class="col-sm-6">
+                      Kode Pos Sesuai Ktp
+                    </label>
+
+                    <div class="col-sm-10">
+                       <input type="input" id="kodepos_ktp_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="alamat_surat" class="col-sm-6">
+                      Alamat Surat Menyurat
+                    </label>
+
+                    <div class="col-sm-10">
+                      <textarea id="alamat_surat_br" class="form-control" rows="3"  /></textarea>
+                    </div>
+                  </div>
+
+
+                   <div class="form-group">
+                    <label for="lurah_surat" class="col-sm-6">
+                      Kelurahan Surat Menyurat
+                    </label>
+                    <div class="col-sm-10">
+                       <input type="input" id="lurah_surat_br" class="form-control"  />
+                    </div>
+                    
+                  </div>
+
+                  <div class="form-group">
+                    <label for="kota_surat" class="col-sm-6">
+                      Kota Surat Menyurat
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" id="kota_surat_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="telp_surat" class="col-sm-6">
+                      No Telepon Surat Menyurat
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="tel" id="telp_surat_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="kec_surat" class="col-sm-6">
+                      Kecamatan Surat Menyurat
+                    </label>
+                    <div class="col-sm-10">
+                      <input type="input" id="kec_surat_br" class="form-control"  /> 
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="kodepos_surat" class="col-sm-6">
+                      Kodepos  Surat Menyurat
+                    </label>
+
+                    <div class="col-sm-10">
+                      <input type="input" id="kodepos_surat_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="no_hp" class="col-sm-4">
+                    No Handphone
+                    </label>
+                    
+                    <div class="col-sm-10">
+                       <input type="tel" id="no_hp_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label for="marital" class="col-sm-4">
+                        Status Pernikahan
+                      </label>
+
+                      <div class="col-sm-10">
+                          <div class="dropdown">
+                          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Dropdown
+                            <span class="caret"></span>
+                          </button>
+                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1" id="matial_br"  />
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                          </ul>
+                      </div>
+                      </div>
+                
+                  </div>
+
+                  <div class="form-group">
+                    <label for="tgl_nikah" class="col-sm-6">
+                      Tanggal Status Pernikahan
+                    </label>
+
+                    <div class="col-sm-10">
+                        <input type="date" id="tgl_nikah_br" class="form-control"  />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="no_kk" class="col-sm-6">
+                      No Kartu Keluarga
+                    </label>
+
+                    <div class="col-sm-10">
+                        <input type="date" id="no_kk_br" class="form-control"  />
+                    </div>
+                     
+                  </div>                     
+</div>
---
+++ resources/views/recruitment/editheader.blade.php
@@ -0,0 +1,51 @@
+   <div class="col-md-12">
+      <div class="row">
+        <div class="col-md-6">
+
+            <div class="form-group">
+                <label for="nik_nasional">
+                  Nik Nasional
+                </label>
+
+                <input type="input" class="form-control" id="nik_nasional" />
+            </div>
+            <div class="form-group">
+                <label for="Nik Sap">
+                  Nik Sap
+                </label>
+                <input type="input" class="form-control" id="nik_sap" />
+            </div>
+            <div class="form-group"> 
+                <label for="Nama Karyawan">
+                  Nama Karyawan
+                </label>
+                <input type="input" class="form-control" id="nama_karyawan" />
+            </div>
+            <div class="form-group"> 
+                <label for="Jabatan">
+                  Jabatan
+                </label>
+                <input type="input" class="form-control" id="jabatan"  disabled />
+            </div>
+            <div class="form-group"> 
+                <label for="tanggal">
+                   Tanggal Aktif Kerja
+                </label>
+                <input type="date" class="form-control" id="tanggal_aktif"  disabled />
+            </div>
+            
+        </div>
+        <div class="col-md-6 center-block">
+
+          <div class="form-group"> 
+                <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" class="img-responsive center-block"  /> 
+               
+                <div class="col-sm-10 col-md-offset-4">
+                     <input type="file" id="foto" value ="">      
+                </div>
+                
+         </div>
+        </div>
+          
+        </div>
+      </div>
